da1c566dbb156164c0b0175068e5d5465a89aae3
clodon2 <waffleme@mail.com>
2024-08-22 14:53:11 -0400

    update comments

--- neuron.py
+++ neuron.py
@@ -2,12 +2,12 @@
 
 
 class Neuron:
-    def __init__(self, input, weight, bias):
+    def __init__(self, inputs, weights, bias):
         """
-        :param input: float list of input values
-        :param weight: float list of weights for values
+        :param inputs: float list of input values
+        :param weights: float list of weights for values
         :param bias: int bias applied to node
         """
-        self.input = input
-        self.weight = weight
+        self.inputs = inputs
+        self.weights = weights
         self.bias = bias
