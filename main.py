601c74f3ce7b38d87adcc552ae68b99db03dffb9
clodon2 <waffleme@mail.com>
2024-08-22 14:47:33 -0400

    update main to utilize neuron class

--- main.py
+++ main.py
@@ -5,8 +5,11 @@ Creating a neural network from scratch
 """
 
 # sample inputs
+from neuron import Neuron
+
 
 inputs = [1.1, 5.6, 9.3]
 weights = [4.2, 10, 7.6]
 bias = 2
 
+myNeuron = Neuron(inputs, weights, bias)
